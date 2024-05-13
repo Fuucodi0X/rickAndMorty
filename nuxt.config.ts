7b4d0080f6207b397f08306b2f27a02cbf06bbb2
Fuucodi0X <akmelf20@gmail.com>
2024-05-13 10:12:09 +0300

    package erro(network issue)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/apollo',
+  ],
+
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint: 'http://localhost:4000',
+      }
+    }
+  }
 })
